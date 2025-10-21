c7068487bb2f7fd2556448d333e1072e7fabe24a
Lucas <lucassidotti@gmail.com>
2025-10-21 10:55:22 -0300

    hoy

--- main.java
+++ main.java
@@ -1,6 +1,24 @@
+/* 
 public class main {
 
     public static void main(String[] args) {
+        //El bloque de codigo que se ejecuta despues de la entrada al programa
         System.out.println("Hola mundo");
+    }
+}
+//Variables
+//int (declara valores enteros) int edad=31
+//double (valores decimales) double pi=3.14
+//boolean (valores booleanos) boolean estudiante=true
+//string (valores tipo texto) string="Lucas"
+
+*/
+
+public class main {
+
+    public static void main(String[] args) {
+        String nombre = "Lucas";
+        int edad = 31;
+        boolean estudiante = true;
     }
 }
